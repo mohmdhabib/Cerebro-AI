c38db2913583ebaad357b406619cec2be70d16b4
johnwesley755 <johnwesley8113@gmail.com>
2025-09-15 17:36:16 +0530

    vercel files are updated now

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: "/",
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
